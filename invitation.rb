0525fe6952679c1fe39ee59184c0d466380e2769
amulyamari <amulya@saimail.com>
2018-07-23 20:41:27 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -2,8 +2,8 @@ puts "What is your name?"
 name= gets.chomp
 puts "What type of party is this?"
 party= gets.chomp
-puts="When is it?"
+puts "When is it?"
 date=gets.chomp
-puts="Who is the host?"
+puts "Who is the host?"
 host=gets.chomp
-puts="Dear #{name}, You are cordially invited to #{party} on #{date}. Please RSVP no later than October 30. Sincerely, #{host}"
+puts "Dear #{name}, You are cordially invited to #{party} on #{date}. Please RSVP no later than October 30. Sincerely, #{host}"
