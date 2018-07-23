1a42ce945e796f54dcc3f1dfacdff54d9a248106
amulyamari <amulya@saimail.com>
2018-07-23 20:36:16 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,5 +1,6 @@
-name="Hermione"
-party="Best Halloween Party Ever"
+puts "What is your name?"
+name= gets.chomp
+puts "What type of party is this?"
 date= "October 31 at 6pm"
 host="Harry Potter"
 puts="Dear #{name}, You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30. Sincerely, Harry Potter"
